2c1b37b09c8635ff856b176afe03ea7b4fe08dea
Md. Shah Samir <mdshahsamir@gmail.com>
2024-10-18 00:53:44 +0600

    Init project

---
+++ app/src/main/java/com/example/android_engineer_test/ui/home/HomeScreen.kt
@@ -0,0 +1,23 @@
+package com.example.android_engineer_test.ui.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.android_engineer_test.ui.theme.AndroidEngineerTestTheme
+
+@Composable
+fun HomeScreen() {
+    HomeScreenContent()
+}
+
+@Composable
+fun HomeScreenContent() {
+
+}
+
+@Preview
+@Composable
+fun HomeScreenContentPreview() {
+    AndroidEngineerTestTheme {
+        HomeScreenContent()
+    }
+}
